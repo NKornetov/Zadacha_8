b9a70cebe5286e8352311f83d60f7afb8be6ffdd
Nikolay Kornetov <nkorntov@yandex.ru>
2019-07-22 02:02:47 +0300

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,10 @@
+public class main {
+    public static void main(String[] args) {
+
+
+        for (int num = 1; num <= 50; num++) {
+
+            System.out.print(num + " ");
+        }
+    }
+}
